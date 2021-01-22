74fde8ab9d42394b52ad794e34657e6acc7844cb
Humair Khan <humair88@hotmail.com>
2021-01-21 22:08:08 -0500

    Added starter site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,78 @@
 import React from "react"
-import { graphql } from 'gatsby'
 
-const HomePage = ({data}) => {
-  return <div>
-    {data.allContentfulTestimonials.edges[0].node.testimonial.testimonial}
-  </div>
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
+import Menu from '../components/menu/menu'
+import Instagram from '../components/instagram'
+import LeafletMap from '../components/leafletMap'
+import Footer from '../components/footer'
+
+const IndexPage = ({data}) => {
+
+return (
+  <Layout>
+    <SEO title="Home" />
+    <section className="hero">
+      <Img fluid={data.headImage.childImageSharp.fluid} className="hero-image"/>
+    </section>
+    <section className="container">
+      <span className="description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </span>
+      <div className="gallery-grid">
+        <Img fluid={data.latte.childImageSharp.fluid} className="gallery-img1"/>
+        <Img fluid={data.coffeeBags.childImageSharp.fluid} className="gallery-img2"/>
+        <Img fluid={data.coffeePortrait.childImageSharp.fluid} className="gallery-img3"/>
+      </div>
+      <div className="about-grid">
+        <h2>WE'RE CRAZY ABOUT COFFEE</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </section>
+    <div className="parallax"></div>
+    <section className="container">
+      <Menu />
+      <Instagram />
+    </section>
+    {typeof window !== 'undefined' &&
+      <LeafletMap
+        position={[24.8132366,67.0400635]} // Your Coordinates 24.8132366,67.0400635,20z
+        zoom={20} // Zoom Level
+        markerText={"Local Cafe, 65 Park Row"} // Icon text
+      />
+    }
+    <Footer />
+  </Layout>
+  )
 }
 
-export const query = graphql`
-query MyQuery {
-  allContentfulTestimonials {
-    edges {
-      node {
-        testimonial {
-          testimonial
-        }
+export const fluidImage = graphql`
+  fragment fluidImage on File {
+    childImageSharp {
+      fluid(maxWidth: 1600) {
+        ...GatsbyImageSharpFluid
       }
     }
   }
-}
-`
+`;
 
-export default HomePage
+export const pageQuery = graphql`
+  query {
+    headImage: file(relativePath: { eq: "header-background.jpg" }) {
+      ...fluidImage
+    }
+    coffeePortrait: file(relativePath: { eq: "coffee-portrait.jpg" }) {
+      ...fluidImage
+    }
+    latte: file(relativePath: { eq: "latte.jpg" }) {
+      ...fluidImage
+    }
+    coffeeBags: file(relativePath: { eq: "coffee-bags.jpg" }) {
+      ...fluidImage
+    }
+  }
+`;
+
+export default IndexPage
