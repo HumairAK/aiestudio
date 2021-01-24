4adfc9a62e171c3504b3b7870e1e8fb207bfd318
Humair Khan <humair88@hotmail.com>
2021-01-23 21:19:15 -0500

    Menu working

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -4,6 +4,7 @@ import Lunch from './lunch'
 import HotDrinks from './hot-drinks'
 import ColdDrinks from './cold-drinks'
 import {graphql, useStaticQuery} from "gatsby";
+import hash from 'object-hash'
 
 const Menu = () => {
     const data = useStaticQuery(graphql`
@@ -19,34 +20,47 @@ query menuQuery {
     }
   }
 } `)
-
     const items = data.allContentfulServices.edges;
+    // items.map(({node}) => {
+    //     console.log(hash(node.name))
+    // })
 
+    const [menuCategory, setMenuCategory] = useState(items[0].node.id);
+    let selectedNode = items.filter(({ node }) => {return node.id === menuCategory})[0].node;
 
-    const [menuCategory, setMenuCategory] = useState('hotDrinks');
-    let SelectedCategory;
-
-
-    if (menuCategory === 'hotDrinks' ) {
-        SelectedCategory = HotDrinks;
-    } else if (menuCategory === 'coldDrinks') {
-        SelectedCategory = ColdDrinks
-    } else if (menuCategory === 'breakfast') {
-        SelectedCategory = Breakfast
-    } else if (menuCategory === 'lunch') {
-        SelectedCategory = Lunch
-    }
+    console.log(selectedNode)
+    selectedNode.items.map( ( item, idx ) => {
+        console.log(item.toString().split(";"))
+    })
 
     return (
         <section className="menu">
             <h2>OUR MENUS</h2>
             <ul className="menu-headers">
-                <li className="menu-header" onClick={() => setMenuCategory('hotDrinks')}>Hot Drinks</li>
-                <li className="menu-header" onClick={() => setMenuCategory('coldDrinks')}>Cold Drinks</li>
-                <li className="menu-header" onClick={() => setMenuCategory('breakfast')}>Breakfast</li>
-                <li className="menu-header" onClick={() => setMenuCategory('lunch')} >Lunch</li>
+                {
+                    items.map(({ node }) => {
+                        return (
+                            <li key={node.id} className="menu-header" onClick={() => setMenuCategory(node.id)}>{node.name}</li>
+                        )
+                    })
+                }
             </ul>
-            <SelectedCategory />
+            <div>
+                <p className="sample">{selectedNode.description}</p>
+                <ul className="menu-items-grid">
+                    {
+                        selectedNode.items.map(( item, idx ) => {
+                            let nameAndPrice = item.toString().split(";")
+                            return (
+                                <li key={idx} className="menu-item">
+                                    <h3>{nameAndPrice[1]}</h3>
+                                    <span>{nameAndPrice[0]}</span>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
         </section>
     )
 }
