02a587060c767a0306a7e30bc8f9891e66547fb9
Humair Khan <humair88@hotmail.com>
2021-01-25 22:20:46 -0500

    Update

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -5,13 +5,13 @@ import {useStaticQuery, graphql} from "gatsby";
 const Gallery = () => {
   const data = useStaticQuery(graphql`
   query {
-    coffeePortrait: file(relativePath: { eq: "coffee-portrait.jpg" }) {
+    coffeePortrait: file(relativePath: { eq: "gallery-3.jpg" }) {
       ...fluidImage
     }
-    latte: file(relativePath: { eq: "latte.jpg" }) {
+    latte: file(relativePath: { eq: "gallery-1.jpg" }) {
       ...fluidImage
     }
-    coffeeBags: file(relativePath: { eq: "coffee-bags.jpg" }) {
+    coffeeBags: file(relativePath: { eq: "gallery-2.jpg" }) {
       ...fluidImage
     }
   }
